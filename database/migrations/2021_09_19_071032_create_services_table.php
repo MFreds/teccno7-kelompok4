9cb75a87ce689d7085ef53b235b5977458fd17d2
rafi nizar <rafi.nizar15@gmail.com>
2021-09-19 16:21:42 +0700

    migrasi jasa dan produk

---
+++ database/migrations/2021_09_19_071032_create_services_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateServicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid');
+            $table->string('name');
+            $table->text('photo');
+            $table->text('description');
+            $table->unsignedBigInteger('price');
+            $table->integer('max_revision');
+            
+            $table->string('status')->default('open');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('services');
+    }
+}
